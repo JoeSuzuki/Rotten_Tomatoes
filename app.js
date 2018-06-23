31449ea054719a028099882b163b5c6a7e584c3a
JoeSuzuki <joesuzuki516@gmail.com>
2018-06-23 01:17:10 -0400

    Installed Mongoose

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Awesome Review" },
+  { title: "Next Review" }
+]
+
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
-})
-
-// OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
-
-// INDEX
-app.get('/reviews', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
 })
