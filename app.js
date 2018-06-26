dc9491ee8ad7f60110eab84136946dfc4b014afd
JoeSuzuki <joesuzuki516@gmail.com>
2018-06-26 16:39:33 -0400

    Review Model

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = express()
 const mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
 
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/rotten-tomatoes', { useMongoClient: true });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+const Review = mongoose.model('Review', {
+  title: String
+});
 
 // OUR MOCK ARRAY OF PROJECTS
 let reviews = [
