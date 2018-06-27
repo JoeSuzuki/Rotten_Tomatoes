d7df5b141a563dd96330d27f4eaaa2ad45d7d9cf
JoeSuzuki <joesuzuki516@gmail.com>
2018-06-27 01:23:59 -0400

    Query through MongoDB for review object

--- app.js
+++ app.js
@@ -8,20 +8,22 @@ mongoose.connect('mongodb://localhost/rotten-tomatoes', { useMongoClient: true }
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
 const Review = mongoose.model('Review', {
   title: String
 });
 
-// OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Awesome Review" },
-  { title: "Next Review" }
-]
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Awesome Review" },
+//   { title: "Next Review" }
+// ]
 
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find().then((reviews) => {
+    res.render('reviews-index', {reviews: reviews});
+  }).catch((err) => {
+    console.log(err);
+  })
 })
 
 app.listen(3000, () => {
